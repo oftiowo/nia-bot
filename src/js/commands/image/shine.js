3e2650fe57af44406dbefd4933440c8b51a0c226
trealable <trealable@yandex.ru>
2017-10-19 12:52:15 +0700

    more images

---
+++ src/js/commands/image/shine.js
@@ -0,0 +1,13 @@
+require(`rootpath`)();
+const ImageResponse = require(`src/js/structures/image-response.js`);
+
+class Shine extends ImageResponse {
+	constructor(text) {
+		super(text);
+		this.command = `shine`;
+		this.category = `image`;
+		this.image = `https://media.discordapp.net/attachments/321605040538386453/370441433791987712/ygeoWFc.png`;
+	}
+}
+
+module.exports = Shine;
